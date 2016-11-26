8e43dde209eec5a9f1fdffffcf1530c3d0455949
Gábor Udvari <gabor.udvari@gmail.com>
2016-11-26 22:59:56 +0100

    Move blocks to sidebar_first

--- web/profiles/wesnoth_hu/wesnoth_hu_after.php
+++ web/profiles/wesnoth_hu/wesnoth_hu_after.php
@@ -148,6 +148,27 @@ $filters->removeInstanceID('filter_null');
 // $filters->removeFilter('filter_null');
 $format->save();
 
+// Set imported block positions
+// user_6 = login
+\Drupal::configFactory()->getEditable('block.block.user_6')
+	->set('region', 'sidebar_first')
+	->save();
+// user_7 = local tools
+\Drupal::configFactory()->getEditable('block.block.user_7')
+	->set('region', 'sidebar_first')
+	->save();
+// user_8 = latest members
+\Drupal::configFactory()->getEditable('block.block.user_8')
+	->set('region', 'sidebar_first')
+	->save();
+// user_9 = who is online
+\Drupal::configFactory()->getEditable('block.block.user_9')
+	->set('region', 'sidebar_first')
+	->save();
+// search1 = search
+\Drupal::configFactory()->getEditable('block.block.search_1')
+	->set('region', 'sidebar_first')
+	->save();
 
 // Create 'alakhu' editor.
 /*
