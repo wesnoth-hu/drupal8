e48dbc057d08c0f0989e60f5f1292f726bbfa35e
Gábor Udvari <gabor.udvari@gmail.com>
2018-04-15 20:02:51 +0200

    Add drush config because of the --uninstall error

---
+++ drupal6/web/sites/all/drush/drushrc.php
@@ -0,0 +1,4 @@
+<?php
+// Due to pm-disable --uninstall, as taken from:
+// https://drupal.stackexchange.com/questions/137671/drush-site-upgrade-unknown-option-uninstall
+$options['strict'] = FALSE;
